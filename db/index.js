3a4c7897e18e98e2f129d313085629013ee1513d
Robby Bacus <robbybacus@gmail.com>
2022-11-22 14:14:11 -0500

    added functions to db/users

--- db/index.js
+++ db/index.js
@@ -1,4 +1,6 @@
 module.exports = {
+  ...require("./cart_products"),
+  ...require("./cart"),
   ...require("./client"),
   ...require("./products"),
   ...require("./users"),
--- db/users.js
+++ db/users.js
@@ -1,16 +1,22 @@
 const client = require("./client");
 
-async function createUser({ first_name, last_name, password, email }) {
+async function createUser({
+  first_name,
+  last_name,
+  password,
+  email,
+  is_admin,
+}) {
   try {
     const {
       rows: [user],
     } = await client.query(
       `
-      INSERT INTO users(first_name, last_name, password, email)
-      VALUES($1, $2, $3, $4)
+      INSERT INTO users(first_name, last_name, password, email, is_admin)
+      VALUES($1, $2, $3, $4, $5)
       ON CONFLICT (email) DO NOTHING
       RETURNING *;`,
-      [first_name, last_name, password, email]
+      [first_name, last_name, password, email, is_admin]
     );
 
     return user;
@@ -19,4 +25,23 @@ async function createUser({ first_name, last_name, password, email }) {
   }
 }
 
-module.exports = { createUser };
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+        SELECT * 
+        FROM users
+        WHERE id=${id}`);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { createUser, getUserById };
