f74f8226683240b55dccbaa44e6ebc4348b52cf5
Robby Bacus <robbybacus@gmail.com>
2022-11-22 13:59:39 -0500

    reorganize db layout

---
+++ db/cart_products.js
@@ -0,0 +1,15 @@
+const client = require("./client");
+
+async function deleteProductFromCart(productId) {
+  try {
+    await client.query(`
+    DELETE
+    FROM cart_products
+    WHERE product_id=${productId}
+    RETURNING * `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { deleteProductFromCart };
---
+++ db/client.js
@@ -0,0 +1,6 @@
+const { Client } = require("pg");
+
+const client = new Client("postgres://localhost:5432/capstoneDev");
+//ssl? do we need to add that before deployment
+
+module.exports = client;
