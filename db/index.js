7b5beb8acfa702c24d8a20f7d85fe3a8fe6a8f25
Robby Bacus <robbybacus@gmail.com>
2022-11-22 11:45:08 -0500

    createUser

--- db/index.js
+++ db/index.js
@@ -1,8 +1,28 @@
-const { Client } = require("pg")
+const { Client } = require("pg");
 
-const client = new Client("postgres://localhost:5432/capstoneDev")
+const client = new Client("postgres://localhost:5432/capstoneDev");
 //ssl? do we need to add that before deployment
 
-module.exports = {
-    client
+async function createUser({ first_name, last_name, password, email }) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    INSERT INTO users(first_name, last_name, password, email)
+    VALUES($1, $2, $3, $4)
+    ON CONFLICT (email) DO NOTHING
+    RETURNING *;`,
+      [first_name, last_name, password, email]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
+
+module.exports = {
+  createUser,
+  client,
+};
