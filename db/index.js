bc7fbd3254e95035b5c105b46425145a822d2b4a
Robby Bacus <robbybacus@gmail.com>
2022-11-22 12:21:35 -0500

    db/index.js pre-lunch push

--- db/index.js
+++ db/index.js
@@ -22,7 +22,94 @@ async function createUser({ first_name, last_name, password, email }) {
   }
 }
 
+async function createProduct({
+  name,
+  description,
+  price,
+  image_url,
+  inventory,
+}) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
+        INSERT INTO products(name, description, price, image_url, inventory)
+        VALUES($1, $2, $3, $4, $5)
+        ON CONFLICT (name) DO NOTHING
+        RETURNING *;`,
+      [name, description, price, image_url, inventory]
+    );
+
+    return product;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllProducts() {
+  try {
+    const { rows: productIds } = await client.query(`
+        SELECT id
+        FROM products`);
+
+    const products = await Promise.all(
+      productIds.map((product) => getProductById(product.id))
+    );
+    return products;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getProductById(productId) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
+        SELECT *
+        FROM products
+        WHERE id=$1`,
+      [productId]
+    );
+
+    return product;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getProductByName(productName) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
+          SELECT *
+          FROM products
+          WHERE name=$1`,
+      [productName]
+    );
+
+    return product;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateProduct({ productId }) {
+  try {
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   createUser,
+  createProduct,
+  getAllProducts,
+  getProductById,
+  getProductByName,
   client,
 };
