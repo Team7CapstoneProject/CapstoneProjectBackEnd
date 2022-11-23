1f015cd5155ed45d6922e64ac4eec2fc014e12ef
Robby Bacus <robbybacus@gmail.com>
2022-11-23 15:26:35 -0500

    working on cart api

--- api/cart.js
+++ api/cart.js
@@ -1,7 +1,35 @@
 const express = require("express");
 const { getAllCarts } = require("../db");
 const cartRouter = express.Router();
+const { createCart } = require("../db");
 
 const { requireUser } = require("./utils");
 
-//GET /api/:userId/cart --------------------------------------------------------- 
+//POST /api/cart/:userId ---------------------------------------------------------
+// cartRouter.post("/:user_id", requireUser, async (req, res, next) => {
+//   const { user_id } = req.params;
+
+
+// const allUserCarts = await getAllCarts()
+// const userCarts = allUserCarts.filter((cart)=>cart.user_id=user_id)
+
+// if(!userCarts.length>1){}
+
+//   if(req.body)
+
+
+
+//   try {
+//     const cart = await createCart({user_id});
+
+//     res.send({
+//       cart
+//     });
+//   } catch ({ name, message }) {
+//     next({ name, message });
+//   }
+// });
+
+//POST /
+
+module.exports = cartRouter;
