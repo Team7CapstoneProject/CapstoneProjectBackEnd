4a0ab8357a8a669c6a8fad8a17d3ec714eaf9a5e
Robby Bacus <robbybacus@gmail.com>
2022-11-25 13:41:10 -0500

    working DELETE api/cart

--- api/cart.js
+++ api/cart.js
@@ -4,19 +4,21 @@ const {
   canEditCartProduct,
   getCartProductByCart,
   addProductToCart,
+  getCartById,
+  deleteCart,
 } = require("../db");
 const cartRouter = express.Router();
 const { requireUser } = require("./utils");
 
 //ADD PRODUCT TO CART : NOT WORKING
-//POST /api/carts/:cartId/products
+//POST /api/carts/:cartId/products------------------------------------------------
 cartRouter.post("/:cartId/products", requireUser, async (req, res, next) => {
   const { cartId } = req.params;
   const { product_id, quantity } = req.body;
 
   //Gets the cart and arrays all the products
   const cartArray = await getCartProductByCart(cartId);
-  console.log(cartArray, "this is cart array")
+  console.log(cartArray, "this is cart array");
   //Declare exists variable
   let exists = false;
   //Goes through all the products in the cart and checks if the product that's being put in already exists
@@ -45,6 +47,30 @@ cartRouter.post("/:cartId/products", requireUser, async (req, res, next) => {
     }
   } catch (error) {
     next();
+  }
+});
+
+//DELETE CART : WORKING
+//DELETE /api/carts/:cartId----------------------------------------------------
+cartRouter.delete("/:cartId", requireUser, async (req, res, next) => {
+  const { cartId } = req.params;
+
+  let cart = await getCartById(cartId);
+  let cartOwner = cart.user_id;
+
+  try {
+    if (cartOwner !== req.user.id) {
+      res.status(403);
+      next({
+        name: "OwnerUserError",
+        message: `User ${req.user.first_name} is not allowed to delete cart with ID ${cartId}`,
+      });
+    } else {
+      let deletedCart = await deleteCart(cartId);
+      res.send(deletedCart);
+    }
+  } catch (error) {
+    throw error;
   }
 });
 
