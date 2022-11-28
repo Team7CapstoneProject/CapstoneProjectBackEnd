87eaec7cf3bac6df36be91230c887eac627f9045
Grant <grant.mc.foster@gmail.com>
2022-11-28 10:26:35 -0600

    troubleshooting

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg");
 console.log("information here!!",process.env.DATABASE_URL)
-const client = new Client("postgres://capstone_dev_user:s0eTYe1iQ8WUMCHz3t7S8ojDEwoFFOv1@dpg-ce2cul9a6gdsa64pur70-a.ohio-postgres.render.com/capstone_dev_5ijo" || "postgres://localhost:5432/capstoneDev");
+const client = new Client("postgres://capstone_dev_user:s0eTYe1iQ8WUMCHz3t7S8ojDEwoFFOv1@dpg-ce2cul9a6gdsa64pur70-a.ohio-postgres.render.com/capstone_dev_5ijo?ssl=true" || "postgres://localhost:5432/capstoneDev");
 //ssl? do we need to add that before deployment
 
 module.exports = client;
