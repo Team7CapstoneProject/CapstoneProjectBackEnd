ff8a5765edb9ec350e7835a59cd9d1355fb18e7c
Grant <grant.mc.foster@gmail.com>
2022-11-28 09:42:20 -0600

    added a build script and adjusted process.env for deployment

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg");
 
-const client = new Client("postgres://localhost:5432/capstoneDev");
+const client = new Client(process.env.DATABASE_URL || "postgres://localhost:5432/capstoneDev");
 //ssl? do we need to add that before deployment
 
 module.exports = client;
