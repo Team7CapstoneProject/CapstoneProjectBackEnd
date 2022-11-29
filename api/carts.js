e8f29b07bb1155ce2bbd8a2483da5fb78f9a3808
Robby Bacus <robbybacus@gmail.com>
2022-11-29 01:35:04 -0500

    additional api/carts routes

--- api/carts.js
+++ api/carts.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const {
-  getAllCarts,
-  canEditCartProduct,
-  getCartProductByCart,
   addProductToCart,
-  getCartById,
+  createCart,
   deleteCart,
+  getCartByEmail,
+  getCartById,
+  getCartProductByCart,
+  updateCartCompletion,
+  getCartsByUserId,
 } = require("../db");
 const cartRouter = express.Router();
 const { requireUser } = require("./utils");
 
 //ADD PRODUCT TO CART : WORKING
-//POST /api/carts/:cartId/products------------------------------------------------
+//POST /api/carts/:cart_id/products------------------------------------------------
 cartRouter.post("/:cart_id/products", requireUser, async (req, res, next) => {
   const { cart_id } = req.params;
   const { product_id, quantity } = req.body;
@@ -48,6 +50,59 @@ cartRouter.post("/:cart_id/products", requireUser, async (req, res, next) => {
   }
 });
 
+
+//CREATES NEW CART : WORKING
+// POST /api/carts/:userId ---------------------------------------------------------
+cartRouter.post("/:user_id", requireUser, async (req, res, next) => {
+  const { user_id } = req.params;
+
+  try {
+    const cart = await createCart({ user_id });
+    res.send(cart);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+//GET MY CART BY EMAIL : WORKING
+// GET /api/carts/myCart ---------------------------------------------------------
+cartRouter.get("/myCartByEmail", requireUser, async (req, res, next) => {
+  try {
+    let email = req.user.email;
+    const cart = await getCartByEmail(email);
+    console.log(cart, `this is all carts by ${req.user.first_name}`);
+    res.send(cart);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//GET MY CART BY USER ID: WORKING
+// GET /api/carts/myCartByUserId---------------------------------------------------------
+cartRouter.get("/myCartByUserId", requireUser, async (req, res, next) => {
+  try {
+    let user_id = req.user.id
+    console.log(req.user, "this is req.user")
+    const cart = await getCartsByUserId(user_id);
+    console.log(cart, `this is all carts by ${req.user.first_name}`);
+    res.send(cart);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//UPDATE CART COMPLETION : WORKING
+// PATCH /api/carts/:cart_id ---------------------------------------------------------
+cartRouter.patch("/:cart_id", requireUser, async (req, res, next) => {
+  const { cart_id } = req.params;
+  try {
+    let updatedCartCompletion = await updateCartCompletion(cart_id);
+    res.send(updatedCartCompletion);
+  } catch (error) {
+    next(error);
+  }
+});
+
 //DELETE CART : WORKING
 //DELETE /api/carts/:cartId----------------------------------------------------
 cartRouter.delete("/:cartId", requireUser, async (req, res, next) => {
@@ -71,29 +126,5 @@ cartRouter.delete("/:cartId", requireUser, async (req, res, next) => {
     throw error;
   }
 });
-
-//POST /api/cart/:userId ---------------------------------------------------------
-// cartRouter.post("/:user_id", requireUser, async (req, res, next) => {
-//   const { user_id } = req.params;
-
-// const allUserCarts = await getAllCarts()
-// const userCarts = allUserCarts.filter((cart)=>cart.user_id=user_id)
-
-// if(!userCarts.length>1){}
-
-//   if(req.body)
-
-//   try {
-//     const cart = await createCart({user_id});
-
-//     res.send({
-//       cart
-//     });
-//   } catch ({ name, message }) {
-//     next({ name, message });
-//   }
-// });
-
-//POST /
 
 module.exports = cartRouter;
--- db/client.js
+++ db/client.js
@@ -1,5 +1,4 @@
 const { Client } = require("pg");
-console.log("information here!!",process.env.DATABASE_URL)
 const client = new Client("postgres://capstone_dev_user:s0eTYe1iQ8WUMCHz3t7S8ojDEwoFFOv1@dpg-ce2cul9a6gdsa64pur70-a.ohio-postgres.render.com/capstone_dev_5ijo?ssl=true" || "postgres://localhost:5432/capstoneDev");
 //ssl? do we need to add that before deployment
 
