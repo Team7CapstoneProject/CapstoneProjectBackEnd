eeca5799f735e3069c9db1539f1b966f91d537cd
Robby Bacus <robbybacus@gmail.com>
2022-12-06 04:11:45 -0500

    revert to previous state before end of day 12/05

--- db/cart_products.js
+++ db/cart_products.js
@@ -119,9 +119,9 @@ async function getCartProductByCart(cartId) {
     [cartId]
     );
 
-    // const cart = await attachProductsToCart(rows)
+    const cart = await attachProductsToCart(rows)
 
-    return rows;
+    return cart;
   } catch (error) {
     throw error;
   }
